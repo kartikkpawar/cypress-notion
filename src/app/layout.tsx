28493745738db97f8ddaf0177bea869c08e3fc41
kartikkpawar <knpwar789@gmail.com>
2024-01-13 16:39:03 +0530

    schadCn Ui setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  console.log(db);
-
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
