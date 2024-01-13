dfd9bfb18373890cd1df94f7da9226922fd8ac0f
kartikkpawar <knpwar789@gmail.com>
2024-01-13 17:19:38 +0530

    Landing Page Section 2

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,29 +1,53 @@
 import TitleSection from "@/components/landingPage/TitleSection";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import React from "react";
+import React, { Fragment } from "react";
 import AppBanner from "../../../public/appBanner.png";
+import { CLIENTS } from "@/lib/constants";
 
 function HomePage() {
   return (
-    <section className=" overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
-      <TitleSection
-        pill="✨ Your Workspace, Perfected"
-        title="All-In-One Collaboration and Productivity Platform"
-      />
-      <div className="bg-white p-[2px] mt-6 rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]">
-        <Button
-          variant="secondary"
-          className=" w-full rounded-[10px] p-6 text-2xl bg-background"
-        >
-          Get Cypress Free
-        </Button>
-      </div>
-      <div className="md:mt-[-90px] sm:w-full w-[750px] flex justify-center items-center mt-[-40px] relative sm:ml-0 ml-[-50px]">
-        <Image src={AppBanner} alt="Application Banner" />
-        <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10" />
-      </div>
-    </section>
+    <Fragment>
+      <section className=" overflow-hidden px-4 sm:px-6 mt-10 sm:flex sm:flex-col gap-4 md:justify-center md:items-center">
+        <TitleSection
+          pill="✨ Your Workspace, Perfected"
+          title="All-In-One Collaboration and Productivity Platform"
+        />
+        <div className="bg-white p-[2px] mt-6 rounded-xl bg-gradient-to-r from-primary to-brand-primaryBlue sm:w-[300px]">
+          <Button
+            variant="secondary"
+            className=" w-full rounded-[10px] p-6 text-2xl bg-background"
+          >
+            Get Cypress Free
+          </Button>
+        </div>
+        <div className="md:mt-[-90px] sm:w-full w-[750px] flex justify-center items-center mt-[-40px] relative sm:ml-0 ml-[-50px]">
+          <Image src={AppBanner} alt="Application Banner" />
+          <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10" />
+        </div>
+      </section>
+      <section className="realtive">
+        <div className="overflow-hidden flex after:content[''] after:dark:from-brand-dark after:to-transparent after:from-background after:bg-gradient-to-l after:right-0 after:top-0 after:bottom-0 after:w-20 after:absolute after:z-10 before:content[''] before:dark:from-brand-dark before:to-transparent before:from-background before:bg-gradient-to-r before:left-0 before:top-0 before:bottom-0 before:w-20 befor:absolute before:z-10">
+          {[...Array(2)].map((arr) => (
+            <div key={arr} className="flex flex-nowrap animate-slide">
+              {CLIENTS.map((client) => (
+                <div
+                  key={client.alt}
+                  className="relative w-[200px] m-20 shrink-0 flex items-center"
+                >
+                  <Image
+                    src={client.alt}
+                    alt={client.alt}
+                    className="object-contain max-w-none"
+                    width={200}
+                  />
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </section>
+    </Fragment>
   );
 }
 
