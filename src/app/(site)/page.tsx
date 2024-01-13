1c5ac1be434fdaf87bd19c81566daeef6bf03f3b
kartikkpawar <knpwar789@gmail.com>
2024-01-13 17:25:50 +0530

    typo fixes

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -26,20 +26,20 @@ function HomePage() {
           <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10" />
         </div>
       </section>
-      <section className="realtive">
-        <div className="overflow-hidden flex after:content[''] after:dark:from-brand-dark after:to-transparent after:from-background after:bg-gradient-to-l after:right-0 after:top-0 after:bottom-0 after:w-20 after:absolute after:z-10 before:content[''] before:dark:from-brand-dark before:to-transparent before:from-background before:bg-gradient-to-r before:left-0 before:top-0 before:bottom-0 before:w-20 befor:absolute before:z-10">
+      <section className="relative">
+        <div className="overflow-hidden flex after:content[''] after:dark:from-brand-dark after:to-transparent after:from-background after:bg-gradient-to-l after:right-0 after:bottom-0 after:top-0 after:w-20 after:z-10 after:absolute  before:content[''] before:dark:from-brand-dark before:to-transparent before:from-background before:bg-gradient-to-r before:left-0 before:top-0 before:bottom-0 before:w-20 before:z-10 before:absolute">
           {[...Array(2)].map((arr) => (
             <div key={arr} className="flex flex-nowrap animate-slide">
               {CLIENTS.map((client) => (
                 <div
                   key={client.alt}
-                  className="relative w-[200px] m-20 shrink-0 flex items-center"
+                  className=" relative w-[200px] m-20 shrink-0 flex items-center"
                 >
                   <Image
-                    src={client.alt}
+                    src={client.logo}
                     alt={client.alt}
-                    className="object-contain max-w-none"
                     width={200}
+                    className="object-contain max-w-none"
                   />
                 </div>
               ))}
@@ -47,6 +47,7 @@ function HomePage() {
           ))}
         </div>
       </section>
+      {/* 51-56 */}
     </Fragment>
   );
 }
