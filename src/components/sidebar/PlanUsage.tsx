57564ca994be78ca6e0d62d1124902505c662400
kartikkpawar <knpwar789@gmail.com>
2024-01-19 21:25:39 +0530

    Plan Usage

---
+++ src/components/sidebar/PlanUsage.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { MAX_FOLDERS_FREE_PLAN } from "@/lib/constants";
+import { useAppState } from "@/lib/providers/SateProvider";
+import { Subscription } from "@/lib/supabase/supabase.types";
+import React, { useEffect, useState } from "react";
+import { Progress } from "@/components/ui/progress";
+import CypressDiamondIcon from "../icons/cypressDiamongIcon";
+
+interface PlanUsageProps {
+  foldersLength: number;
+  subscription: Subscription | null;
+}
+
+const PlanUsage: React.FC<PlanUsageProps> = ({
+  foldersLength,
+  subscription,
+}) => {
+  const { workspaceId, state } = useAppState();
+  const [usagePercentage, setUsagePercentage] = useState(
+    (foldersLength / MAX_FOLDERS_FREE_PLAN) * 100
+  );
+
+  useEffect(() => {
+    const stateFolderLenght = state.workspaces.find(
+      (worksapce) => worksapce.id === workspaceId
+    )?.folders.length;
+
+    if (!stateFolderLenght) return;
+
+    setUsagePercentage((stateFolderLenght / MAX_FOLDERS_FREE_PLAN) * 100);
+  }, []);
+
+  return (
+    <article className="mb-4">
+      {subscription?.status !== "active" && (
+        <div className="flex gap-2 text-muted-foreground mb-2 items-center">
+          {" "}
+          <div className="h-4 w-4">
+            <CypressDiamondIcon />
+          </div>
+          <div className="flex w-full items-center justify-between">
+            <div className="">Free Plan</div>
+            <small>{usagePercentage.toFixed(0)}% / 100%</small>
+          </div>
+        </div>
+      )}
+      {subscription?.status !== "active" && (
+        <Progress value={usagePercentage} className="h-1" />
+      )}
+    </article>
+  );
+};
+
+export default PlanUsage;
