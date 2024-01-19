475c63d27603052118968f1f5091f2b9e439cdc4
kartikkpawar <knpwar789@gmail.com>
2024-01-19 21:55:05 +0530

    schema changes

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -69,6 +69,13 @@ const Sidebar: React.FC<SidebarProps> = async ({ params, className }) => {
           subscription={subscriptionData}
         />
         <NativeNavigation myWorkspaceId={params.workspaceId} />
+        <ScrollArea className="overflow-scroll relative h-[450px] ">
+          <div className="pointer-events-none w-full absolute bottom-0 h-20 bg-gradient-to-t from-background to-transparent z-40" />
+          <FoldersDropdownList
+            workspaceFolders={workspaceFolderData || []}
+            workspaceId={params.workspaceId}
+          />
+        </ScrollArea>
       </div>
     </aside>
   );
--- src/lib/supabase/schema.ts
+++ src/lib/supabase/schema.ts
@@ -15,7 +15,9 @@ export const workspaces = pgTable("workspaces", {
   createdAt: timestamp("created_at", {
     withTimezone: true,
     mode: "string",
-  }),
+  })
+    .defaultNow()
+    .notNull(),
   workspaceOwner: uuid("workspace_owner").notNull(),
   title: text("title").notNull(),
   iconId: text("icon_id").notNull(),
