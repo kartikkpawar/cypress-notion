00ff6612aa0ea9e06dad9f4ce0502d5fbf9cdfa5
kartikkpawar <knpwar789@gmail.com>
2024-01-26 23:02:39 +0530

    stripe payment_flow

--- src/components/global/SubscriptionModal.tsx
+++ src/components/global/SubscriptionModal.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useSubscriptionModal } from "@/lib/providers/SubscriptionModalProvider";
-import { ProductWirhPrice } from "@/lib/supabase/supabase.types";
+import { Price, ProductWithPrice } from "@/lib/supabase/supabase.types";
 import React, { Fragment, useState } from "react";
 import {
   Dialog,
@@ -12,15 +12,48 @@ import {
 import { useSupabaseUser } from "@/lib/providers/SupabaseUserProvider";
 import { Button } from "../ui/button";
 import Loader from "./Loader";
+import { formatPrice, postData } from "@/lib/utils";
+import { useToast } from "../ui/use-toast";
+import { getStripe } from "@/lib/stripe/stripeClient";
 
 interface SubscriptionModalProps {
-  products: ProductWirhPrice[];
+  products: ProductWithPrice[];
 }
 
-const SubscriptionModal: React.FC<SubscriptionModalProps> = () => {
+const SubscriptionModal: React.FC<SubscriptionModalProps> = ({ products }) => {
   const { open, setOpen } = useSubscriptionModal();
+  const { toast } = useToast();
   const { subscription } = useSupabaseUser();
   const [isLoading, setIsLoading] = useState(false);
+  const { user } = useSupabaseUser();
+
+  const onClickContinue = async (price: Price) => {
+    try {
+      setIsLoading(true);
+      if (!user) {
+        toast({ title: "You must be logged in" });
+        setIsLoading(false);
+        return;
+      }
+      if (subscription) {
+        toast({ title: "Already on a paid plan" });
+        setIsLoading(false);
+        return;
+      }
+      const { sessionId } = await postData({
+        url: "/api/create-checkout-session",
+        data: { price },
+      });
+
+      console.log("Getting Checkout for stripe");
+      const stripe = await getStripe();
+      stripe?.redirectToCheckout({ sessionId });
+    } catch (error) {
+      toast({ title: "Oppse! Something went wrong.", variant: "destructive" });
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
@@ -34,16 +67,28 @@ const SubscriptionModal: React.FC<SubscriptionModalProps> = () => {
           <DialogDescription>
             To access features you need to have a paid plan.
           </DialogDescription>
-          <div className="flex justify-between items-center ">
-            <Fragment>
-              <b className="text-3xl text-foreground">
-                $12.99/ <small>month</small>
-              </b>
-              <Button disabled={isLoading}>
-                {isLoading ? <Loader /> : "Upgrade ✨"}
-              </Button>
-            </Fragment>
-          </div>
+          {products.length
+            ? products.map((product) => (
+                <div
+                  className="flex justify-between items-center"
+                  key={product.id}
+                >
+                  {product.prices?.map((price) => (
+                    <Fragment key={price.id}>
+                      <b className="text-3xl text-foreground">
+                        {formatPrice(price)}/ <small>{price.interval}</small>
+                      </b>
+                      <Button
+                        disabled={isLoading}
+                        onClick={() => onClickContinue(price)}
+                      >
+                        {isLoading ? <Loader /> : "Upgrade ✨"}
+                      </Button>
+                    </Fragment>
+                  ))}
+                </div>
+              ))
+            : ""}
         </DialogContent>
       )}
     </Dialog>
