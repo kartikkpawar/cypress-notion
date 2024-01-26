a73c596cd80e481586c5e6aac825d440776fa275
kartikkpawar <knpwar789@gmail.com>
2024-01-26 23:35:14 +0530

    Trash Flow

--- src/components/sidebar/NativeNavigation.tsx
+++ src/components/sidebar/NativeNavigation.tsx
@@ -5,6 +5,7 @@ import CypressHomeIcon from "../icons/cypressHomeIcon";
 import CypressSettingsIcon from "../icons/cypressSettingsIcon";
 import CypressTrashIcon from "../icons/cypressTrashIcon";
 import Settings from "../settings/Settings";
+import Trash from "../trash/Trash";
 
 interface NativeNavigationProps {
   myWorkspaceId: string;
@@ -33,10 +34,12 @@ const NativeNavigation: React.FC<NativeNavigationProps> = ({
             <span>Settings</span>
           </li>
         </Settings>
-        <li className="group/native flex text-Neutrals/neutrals-7 transition-all gap-2 items-center cursor-pointer">
-          <CypressTrashIcon />
-          <span>Trash</span>
-        </li>
+        <Trash>
+          <li className="group/native flex text-Neutrals/neutrals-7 transition-all gap-2 items-center cursor-pointer">
+            <CypressTrashIcon />
+            <span>Trash</span>
+          </li>
+        </Trash>
       </ul>
     </nav>
   );
